151aa58e21c977b0db617448c47a18aa650abe15
Josh Wisely <joshwisely@gmail.com>
2016-04-25 20:21:44 -0700

    Add index.html file.

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,8 +14,29 @@ class test::install {
   notify { "Installing!": }
   
   class{'nginx':
-      manage_repo => true,
-      package_source => 'nginx-mainline'
+    manage_repo => true,
+    package_source => 'nginx-mainline'
+  }
+  
+  file { '/var/test':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0750',
+  }
+  
+  nginx::resource::vhost { 'test':
+    ensure               => present,
+    server_name          => ['test'],
+    listen_port          => 8000,
+    www_root             => '/var/test',
+  }
+  
+  file { '/var/test/index.html':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => epp('templates/index.html.epp'),
   }
 
 }
